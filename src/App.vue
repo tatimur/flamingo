7a4919e1e8fd9ac6e06ffa97c7820ca4d498a77a
tatimur <bolshakova_tan@mail.ru>
2020-09-21 12:55:44 +0300

    добавила приложение

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="container">
+    <Card />
+    <Card title="flamingo" font_size=5 image_url="https://bipbap.ru/wp-content/uploads/2019/05/1-flamingo-drawing-step-by-step-136.jpg" />
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="wrap">
+    <div class="card" style="width: 18rem;">
+      <img class="card-img" :src="image_url" alt="">
+      <h3 class="card-title" v-bind:style="{fontSize: font_sizeLocal + 'px'}"> {{titleLocal}} </h3>
+    </div>
+    <div class="content">
+      <div class="form-group">
+        <p>title</p>
+        <input class="form-control" v-model="titleLocal" type="text">
+      </div>
+      <div class="form-group">
+        <p>font size:</p>
+        <input class="form-control" v-model="font_sizeLocal" type="number" min="0">
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: {
+      type: String,
+      default: "MENU"
+    },
+    font_size: {
+      type: String,
+      default: "20"
+    },
+    image_url: {
+      type: String,
+      default: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRoaqPDmLsepthNX6Jrc9ptK6JwfQvvk3WuRQ&usqp=CAU"
+    }
+  },
+  data () {
+    return {
+      titleLocal: this.title,
+      font_sizeLocal: this.font_size
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .card {
+    text-align: center;
+  }
+  .wrap {
+    display: flex;
+    margin: 10px;
+  }
+  .content {
+    margin-left: 40px;
+  }
+  .card-img {
+    max-width: 300px;
+  }
+</style>
